dbf36bd54c1b1d1f666bc40572228e23386816d5
srikanthpragada <srikanthpragada@gmail.com>
2022-11-04 18:56:35 +0530

    04-NOV

---
+++ structs/preprocessor.c
@@ -0,0 +1,19 @@
+
+#define SIZE 50
+#define TITLE "Srikanth Technologies"
+// Macro
+#define iseven(n)  n % 2 == 0
+
+void main()
+{
+ int a[SIZE];
+ int i;
+
+    for(i = 0 ; i < SIZE; i ++)
+       ;
+
+    puts(TITLE);
+
+    if(iseven(a))
+        ;
+}
